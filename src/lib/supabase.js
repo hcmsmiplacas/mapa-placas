629ae35f6b1e442fba9947e8dc0f842f4a681711
Henrique Caldeira de Medeiros <henrique.medeiros@gruposmi.local>
2026-05-26 15:09:00 -0300

    config deploy vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jvbyzzbbcmfrcatoahwx.supabase.co'
-const supabaseKey = 'sb_publishable_8HENcXz0A7cG8b6n760kyA__H81sMdh'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+export const supabase = createClient(supabaseUrl, supabaseKey)
